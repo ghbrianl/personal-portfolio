23d2398957b630cdfe40051f6053d6e97037274c
Lin Ni <78550234+linni123789@users.noreply.github.com>
2022-11-27 10:43:25 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-full">
+      <div className="w-full h-32 bg-amber-700">
+        <div className="text-yellow-500 text-7xl text-center relative top-5 first-letter:font-serif">
+          Brian Lu
+        </div>
+      </div>
+      <div>Description</div>
     </div>
   );
 }
